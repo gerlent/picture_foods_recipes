ee5bed53616fd7ad71ef3d8f17caa21b93c2f475
Behsaad Ramez <beh.ramez@gmail.com>
2019-03-16 14:49:29 +0100

    fix relationships

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,6 @@ class Ingredient < ActiveRecord::Base
   include Obfuscatable
   obfuscate_id prefix: 'ingr'
 
-  has_many :recipes, through: :recipe_ingredients
   has_many :recipe_ingredients, dependent: :destroy
+  has_many :recipes, through: :recipe_ingredients, source_type: "Recipe"
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,6 @@ class Recipe < ActiveRecord::Base
   include Obfuscatable
   obfuscate_id prefix: 'rcp'
 
-  has_many :ingredients, through: :recipe_ingredients
   has_many :recipe_ingredients, dependent: :destroy
+  has_many :ingredients, through: :recipe_ingredients, source_type: "Ingredient"
 end
